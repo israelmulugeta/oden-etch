6e0de25b4fc6a857e0916d9de047fbd9c92dcf7a
israelmulugeta <93257066+israelmulugeta@users.noreply.github.com>
2023-03-18 16:54:28 +0300

    completed

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ function incubate(){
 
 function incubate2(){
     let car=document.createElement('div');
-    car.classList='boxes';
+    car.classList=`boxes`;
+    car.id=`box${grid}`
     return car;
 }
 function generator2()
@@ -17,7 +18,7 @@ function generator2()
     //take input
     let input=document.querySelector('.myValue').value;
     if(Number(input)>50||Number(input)<=0){
-        alert('nigga read it says 1 to 50');
+        alert('change your number to something between 1 and 50 ,Thanks');
     }
     //use the input number to do 2 loops one for lines the other for the items on a line
     else if(Number(input)<=50)
@@ -34,9 +35,10 @@ function generator2()
             for(let i=0 ;i<Number(input);i++)
              {
                element.appendChild(incubate2());
+               grid++
              }
         });
-
+       
     } 
 };   
 
@@ -46,5 +48,62 @@ let submit=document.querySelector('.submit');
 submit.addEventListener('click',generator2);
 
 
+let drawer=document.querySelector('.draw');
+let eraser=document.querySelector('.erase');
+let color=document.querySelector('.randomColor');
+let boxes=[...document.querySelectorAll('.boxes')];//frame for how many boxes i have
+
+
+//global eventlistener
+
+
+function adder(){
+  let boxes=[...document.querySelectorAll('.boxes')];//frame for how many boxes i have
+  boxes.forEach(box => {
+    box.addEventListener('mouseover',consoler)
+    function consoler(){
+          box.style.backgroundColor='black';
+    }
+  });
+}
+
+function remover(){
+  let boxes=[...document.querySelectorAll('.boxes')];//frame for how many boxes i have
+  boxes.forEach(box => {
+    box.addEventListener('mouseover',consoler)
+    function consoler(){
+          box.style.backgroundColor='white';
+    }
+  });
+}
+function rando()
+{
+  let a=Math.floor(Math.random()*10);
+  if(a===0){return 'blue'}
+  if(a===1){return 'red'}
+  if(a===2){return 'green'}
+  if(a===3){return 'purple'}
+  if(a===4){return 'brown'}
+  if(a===5){return 'orange'}
+  if(a===6){return 'yellow'}
+  if(a===7){return 'gray'}
+  if(a===8){return 'pink'}
+  if(a===9){return 'gold'}
+ 
+}
+function painter(){
+  let boxes=[...document.querySelectorAll('.boxes')];//frame for how many boxes i have
+  boxes.forEach(box => {
+    box.addEventListener('mouseover',consoler)
+    function consoler(){
+          
+          box.style.backgroundColor=rando();
+    }
+  });
+}
+
+drawer.addEventListener('click',adder);
+eraser.addEventListener('click',remover);
+color.addEventListener('click',painter);
 
 
